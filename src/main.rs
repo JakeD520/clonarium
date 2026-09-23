ad23cd1fe5187f9d8121937c0244d5978a52a3fe
JakeD520 <jd01234@gmail.com>
2026-09-23 16:09:41 -0600

    ldtk retry

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(LdtkPlugin)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // 1. Position camera at the center of your 480x384 level
+    commands.spawn((
+        Camera2d,
+        Transform::from_xyz(240.0, 192.0, 0.0),
+    ));
+
+    // 2. Select Level_0
+    commands.insert_resource(LevelSelection::index(0));
+
+    // 3. Spawn LDtk World
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: LdtkProjectHandle{handle:
+            asset_server.load("maps/starter_base/starter_base.ldtk")
+        },
+        ..Default::default()
+    });
 }
